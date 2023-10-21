808179775174d889c1068664233523d85ab75818
Alexander Shkurlatov <shkurlatov80@gmail.com>
2023-10-21 22:31:30 +1200

    Add Game settings

---
+++ Assets/_Snake/CodeBase/Settings/GameSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Snake.Settings
+{
+    [CreateAssetMenu(menuName = "Settings/GameSettings", fileName = "GameSettings")]
+    public class GameSettings : ScriptableObject
+    {
+        public GameplaySettings GameplaySettings;
+        public SnakeSettings SnakeSettings;
+    }
+}
---
+++ Assets/_Snake/CodeBase/Settings/GameplaySettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Snake.Settings
+{
+    [Serializable]
+    public class GameplaySettings
+    {
+        [Range(1, 200)]
+        public int FeedAmount;
+    }
+}
